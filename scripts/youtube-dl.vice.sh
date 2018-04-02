47a77db4f27515527cb3b13193c24d81250446cd
OMG <omg@orionsbelt.space>
2018-04-02 11:04:45 -0800

    added prinf command
    Added printf command to create initial log file. removed qoutes around paths.

--- scripts/youtube-dl.vice.sh
+++ scripts/youtube-dl.vice.sh
@@ -1,8 +1,9 @@
-youtube-dl --verbose --ignore-errors \
---download-archive '$HOME/vice/archivedytidlist.txt' \
---retries infinite --fragment-retries infinite --buffer-size 16k \
---hls-prefer-ffmpeg -o '$HOME/vice/%(uploader)s/%(uploaddate)s%(title)s.%(ext)s' \
---no-continue --no-part --write-description --write-info-json \
---write-annotations --write-all-thumbnails --console-title --all-subs \
---external-downloader axel --external-downloader-args '-n 15' \
---prefer-ffmpeg https://www.youtube.com/user/vice/videos | tee --append '$HOME/vice/_youtubedl.logfile'
+printf 'go go gadget\n' > $HOME/vice/_youtubedl.logfile | \                                                   
+youtube-dl --verbose --ignore-errors \                 
+--download-archive '$HOME/vice/archivedytidlist.txt' --playlist-random \                                      
+--retries infinite --fragment-retries infinite --buffer-size 16k \                                            
+--hls-prefer-ffmpeg -o '$HOME/vice/%(uploader)s/%(uploaddate)s%(title)s.%(ext)s' \                            
+--no-continue --no-part --write-description --write-info-json \                                               
+--write-annotations --write-all-thumbnails --console-title --all-subs \                                       
+--external-downloader axel --external-downloader-args '-n 15' \                                               
+--prefer-ffmpeg https://www.youtube.com/user/vice/videos | tee --append $HOME/vice/_youtubedl.logfile
